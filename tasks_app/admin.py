fcccac314bb57724b0ac39a94a2f413e620efdf3
pabloquihui <pabloquihui@gmail.com>
2024-04-24 20:05:46 -0600

    chat bulk admin

--- tasks_app/admin.py
+++ tasks_app/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import ScheduledTask, Tasks, TaskExecution
-
+from .models import *
 class ScheduledTaskAdmin(admin.ModelAdmin):
-    list_display = ('custom_name', 'task_name', 'task_type', 'crontab_schedule_display', 'interval_seconds', 'template_name', 'template_namespace', 'chat_id', 'redbeat_key')
+    list_display = ('custom_name', 'task_id', 'bulk_chat_model_id', 'task_name', 'task_type', 'crontab_schedule_display', 'interval_seconds', 'template_name', 'template_namespace', 'chat_id', 'redbeat_key')
     search_fields = ('task_name', 'template_name', 'template_namespace', 'chat_id', 'redbeat_key')
     exclude = ('redbeat_key',)
     def save_model(self, request, obj, form, change):
         """Override save_model method to call save_to_redbeat."""
         obj.save_to_redbeat()
+        
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
         
 class TasksAdmin(admin.ModelAdmin):
     list_display = ('task_name',)
@@ -21,6 +24,23 @@ class TaskExecutionAdmin(admin.ModelAdmin):
     readonly_fields = ['task_name', 'task_id', 'periodic_name', 'execution_type', 'execution_date', 'execution_time', 'status']
     list_filter = ('task_id', 'task_name', 'periodic_name', 'execution_date', 'execution_time', 'status', 'execution_type')
 
+class ChatScheduledTaskAdmin(admin.ModelAdmin):
+    list_display = ('chat_ids', 'id', 'task_name', 'custom_name', 'template_name', 'task_type', 'crontab_schedule_display', 'interval_seconds')  # Campos que se mostrarán en la lista
+    search_fields = ('chat_ids', 'task_name', 'custom_name', 'template_name', 'task_type')  # Campos por los que se puede buscar
+    list_filter = ('chat_ids', 'task_name', 'template_name', 'task_type') 
+    fields = ['chat_ids', 'task_name', 'custom_name', 'template_name', 'template_namespace', 'task_type', 'interval_seconds', 'crontab_minute', 'crontab_hour', 'crontab_day_of_month', 'crontab_month_of_year', 'crontab_day_of_week']
+    
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = list(super().get_readonly_fields(request, obj))
+        if obj:
+            readonly_fields.append('custom_name')
+        return readonly_fields
+    
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+    
 admin.site.register(ScheduledTask, ScheduledTaskAdmin)
 admin.site.register(Tasks, TasksAdmin)
 admin.site.register(TaskExecution, TaskExecutionAdmin)
+admin.site.register(ChatScheduledTask, ChatScheduledTaskAdmin)
